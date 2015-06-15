a349283446d5bc17f6abf1be458f17900c9830e5
William Lachance <wlachance@mozilla.com>
2015-06-15 16:56:56 -0400

    Peg release of treeherder-client used to 1.1
    We actually don't use treeherder-client for anything, but if we download
    the latest version we'll have lots of problems because it's incompatible with
    gaiatest.

--- src/eideticker/setup.py
+++ src/eideticker/setup.py
@@ -7,7 +7,7 @@ setup(
         'mozdevice>=0.36', 'mozlog>=1.5', 'mozprofile>=0.19',
         'moznetwork>=0.24', 'mozhttpd>=0.7', 'BeautifulSoup',
         'gaiatest>=0.22', 'httplib2', 'b2gpopulate>=0.27',
-        'requests>=2.2.1', 'futures >= 2.1.6',])
+        'requests>=2.2.1', 'futures >= 2.1.6', 'treeherder-client == 1.1'])
 
 # (FIXME: templeton needs to be installed manually now, it constantly causes
 # problems)
