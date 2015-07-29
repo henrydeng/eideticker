fce9169299566bec90fee02cd0a15b98d55a3fab
William Lachance <wlachance@mozilla.com>
2015-07-29 12:51:22 -0400

    Pin dependencies

--- src/eideticker/setup.py
+++ src/eideticker/setup.py
@@ -4,9 +4,10 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'mozdevice==0.46', 'mozlog==2.11', 'mozprofile==0.25',
-        'moznetwork==0.27', 'mozhttpd==0.7', 'BeautifulSoup',
-        'gaiatest>=0.22', 'httplib2', 'b2gpopulate>=0.27',
+        'mozdevice==0.45', 'mozlog==2.11', 'mozprofile==0.24',
+        'moznetwork==0.26', 'mozhttpd==0.7', 'mozrunner==6.7',
+        'gaiatest==0.32', 'b2gpopulate==0.27', 'marionette_client==0.8.5',
+        'httplib2', 'BeautifulSoup',
         'requests>=2.2.1', 'futures >= 2.1.6', 'treeherder-client == 1.1'])
 
 # (FIXME: templeton needs to be installed manually now, it constantly causes
